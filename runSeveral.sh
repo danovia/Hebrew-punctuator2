13b7700ea452e0b821965ab5637b5d2036bb699d
Unknown <itay.alfia@gmail.com>
2018-09-14 18:31:38 +0300

    change runSeveral.sh

--- runSeveral.sh
+++ runSeveral.sh
@@ -2,14 +2,12 @@
 
 RESULTS_DIR=./results
 EVALUATIONS_DIR=${RESULTS_DIR}/evaluations
-MODELS_DIR=${RESULTS_DIR}/models
 OUTPUTS_DIR=${RESULTS_DIR}/outputs
 
 INPUT_FILE=./example/out/ep.dev.txt
 
 # Begin code:
 mkdir $EVALUATIONS_DIR
-mkdir $MODELS_DIR
 mkdir $OUTPUTS_DIR
 
 function run_data() {
@@ -32,7 +30,7 @@ function run_model() {
 
     local model_substr=h${hidden}_lr${lr}$([ "$isOneWay" = 1 ] && echo "_oneWay" || echo "")
     local evaluation_file=${EVALUATIONS_DIR}/Evaluation_${model_name}_${model_substr}.txt
-    local model_file=${MODELS_DIR}/Model_${model_name}_${model_substr}.pcl
+    local model_file=Model_${model_name}_${model_substr}.pcl
     local output_file=${OUTPUTS_DIR}/Output_${model_name}_${model_substr}.txt
 
     python main.py $model_name $hidden $lr $isOneWay
@@ -40,5 +38,13 @@ function run_model() {
     python error_calculator.py $INPUT_FILE $output_file > $evaluation_file
 }
 
-run_data ./example/embeddings/wiki.he-morph.window10.fasttext.skipgram-model.vec "./example/convert_table_dataset.sh ./example/training-hebrew/ | python ./example/untransliterate.py"
-run_model 512 0.02 0 morph_fasttext10
+#run_data ./example/embeddings/wiki.he-morph.window10.fasttext.skipgram-model.vec "./example/convert_table_dataset.sh ./example/training-hebrew-morph/ | python ./example/untransliterate.py"
+#run_model 512 0.02 0 morph_fasttext10
+#run_model 256 0.02 0 morph_fasttext10
+#run_model 256 0.02 1 morph_fasttext10
+#run_data ./example/embeddings/wiki.he-morph.window10.word2vec.skipgram-model.vec "./example/convert_table_dataset.sh ./example/training-hebrew-morph/ | python ./example/untransliterate.py"
+#run_model 512 0.02 0 morph_word2vec10
+#run_data ./example/embeddings/wiki.he-regular.window5.fasttext.skipgram-model.vec "cat ./example/training-hebrew-full/*.txt"
+#run_model 512 0.02 0 full_fasttext5
+#run_data ./example/embeddings/wiki.he-regular.window5.word2vec.skipgram-model.vec "cat ./example/training-hebrew-full/*.txt"
+#run_model 512 0.02 0 full_word2vec5
